ca69fb35208b3dcb217cdc40727b9ee1a38ea095
MarcWeberFS <marc.robin.weber@gmail.com>
2024-11-11 04:31:01 +0100

    adjusted python version for ec2 instance local python script

--- video-downloader-backend/src/main/java/ch/marc/delegates/InstagramVideoDownloaderDelegate.java
+++ video-downloader-backend/src/main/java/ch/marc/delegates/InstagramVideoDownloaderDelegate.java
@@ -26,7 +26,7 @@ public class InstagramVideoDownloaderDelegate implements JavaDelegate {
     private String downloadInstagramVideo(String url, String s3BucketName, String s3Folder) {
         String result = "";
         try {
-            String pythonExecutable = "python3";
+            String pythonExecutable = "python";
             String scriptPath = "/home/ec2-user/scripts/video_downloader.py";
 
             ProcessBuilder processBuilder = new ProcessBuilder(
