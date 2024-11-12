48da5c3b43b36660ba6f85a9c74525b2b5910b7b
MarcWeberFS <marc.robin.weber@gmail.com>
2024-11-12 21:35:47 +0100

    Add global variable for source language support to TranscribeVideoDelegate for dynamic language selection

--- video-downloader-backend/src/main/java/ch/marc/delegates/TranscribeVideoDelegate.java
+++ video-downloader-backend/src/main/java/ch/marc/delegates/TranscribeVideoDelegate.java
@@ -13,6 +13,7 @@ public class TranscribeVideoDelegate implements JavaDelegate {
     @Override
     public void execute(DelegateExecution execution) {
         String downloadLink = (String) execution.getVariable("downloadLink");
+        String sourceLanguage = (String) execution.getVariable("sourceLanguage");
 
         String s3Uri = downloadLink.replace("https://video-download-temp.s3.amazonaws.com/", "s3://video-download-temp/");
         String videoId = s3Uri.substring(s3Uri.lastIndexOf("/") + 1, s3Uri.lastIndexOf("."));
@@ -24,7 +25,7 @@ public class TranscribeVideoDelegate implements JavaDelegate {
 
         StartTranscriptionJobRequest transcriptionJobRequest = StartTranscriptionJobRequest.builder()
             .transcriptionJobName(jobName)
-            .languageCode(LanguageCode.EN_US)
+            .languageCode(getLanguageCodeByString(sourceLanguage))
             .mediaFormat(MediaFormat.MP4)
             .media(Media.builder().mediaFileUri(s3Uri).build())
             .subtitles(Subtitles.builder().formats(SubtitleFormat.SRT).build())
@@ -61,6 +62,45 @@ public class TranscribeVideoDelegate implements JavaDelegate {
             e.printStackTrace();
         } finally {
             transcribeClient.close();
+        }
+    }
+
+    private LanguageCode getLanguageCodeByString (String sourceLanguage) {
+        switch (sourceLanguage) {
+            case "en":
+                return LanguageCode.EN_US;
+            case "de":
+                return LanguageCode.DE_DE;
+            case "fr":
+                return LanguageCode.FR_FR;
+            case "es":
+                return LanguageCode.ES_US;
+            case "it":
+                return LanguageCode.IT_IT;
+            case "pt":
+                return LanguageCode.PT_BR;
+            case "nl":
+                return LanguageCode.NL_NL;
+            case "ru":
+                return LanguageCode.RU_RU;
+            case "zh":
+                return LanguageCode.ZH_CN;
+            case "ja": 
+                return LanguageCode.JA_JP;
+            case "ko":
+                return LanguageCode.KO_KR;
+            case "ar":
+                return LanguageCode.AR_SA;
+            case "hi":
+                return LanguageCode.HI_IN;
+            case "tr":
+                return LanguageCode.TR_TR;
+            case "pl":
+                return LanguageCode.PL_PL;
+            case "sv":
+                return LanguageCode.SV_SE;
+            default:
+                return LanguageCode.EN_US;
         }
     }
 }
