0b50bbf10148797b1445cbb8a26eacd783437b09
MarcWeberFS <marc.robin.weber@gmail.com>
2024-11-12 14:39:24 +0100

    setup BPMN for the translation delegate

---
+++ video-downloader-backend/src/main/java/ch/marc/delegates/TranslateVideoDelegate.java
@@ -0,0 +1,17 @@
+package ch.marc.delegates;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TranslateVideoDelegate implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        
+    }
+
+    
+    
+}
